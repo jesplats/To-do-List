3a9545dacdc46df29e054e9045ecfd5c31138afd
jesus_plata <jesusplata1520@gmail.com>
2025-05-05 16:55:16 -0500

    a

--- src/components/TodoAdd.jsx
+++ src/components/TodoAdd.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import { useForm } from '../hooks/useForm'
 // import  {AddTodo} from '../hooks/useTodo'
-export const TodoAdd = () => {
+export const TodoAdd = ({handleNewTodo}) => {
+  
+  
+  const {description,onInputChange,onResetForm}=useForm({
+    description:'',
+  })
+  
+  const onFormSubmit=(e)=>{
+    e.preventDefault()
+    if(description.length<=1) return
+
+    let newTodo={
+      id: new Date().getTime(),
+      description:description,
+      done:false
+    }
+    handleNewTodo(newTodo)
+    onResetForm()
+
+  }
+  
   return (
-    <form>
+    <form onSubmit={onFormSubmit}>
     <input type="text" 
      className="input-update" 
-     value="aprender js" 
+     value={description} 
      name="description"
+      onChange={onInputChange}
      placeholder="Agregar tarea"
      />
    
--- src/components/TodoUpdate.jsx
+++ src/components/TodoUpdate.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
 import { FaEdit } from "react-icons/fa";
+import { useForm } from '../hooks/useForm'
 export const TodoUpdate = ({todo,handleUpdateTodo}) => {
+  
+  const {updateDescription,onInputChange}=useForm({
+    updateDescription:todo.description
+  })
+
+
+  const [disable, setdisable] = useState(true)
+
+  
+  const onSubmitUpdate = (e) => {
+    e.preventDefault();
+    if (updateDescription.length <= 1) return;
+  
+    const id = todo.id; // Cambiado de coma a punto y coma
+    const description = updateDescription; // Cambiado de coma a punto y coma
+  
+    handleUpdateTodo(id, description);
+  };
+  
   return (
-    <form>
+    <form onSubmit={onSubmitUpdate}>
      <input type="text" 
       className="input-update" 
       value={todo.description} 
-      name="description"
+      onChange={onInputChange}
+      name='updateDescription'
       placeholder="Agregar tarea"
       />
     
